59d7215c5fd5d70aca8f20d7cbb86490d4b75b85
iyokurot <01kayochin17neko@gmail.com>
2020-03-13 04:53:46 +0900

    webgl build

--- Assets/Script/SensorSever.cs
+++ Assets/Script/SensorSever.cs
@@ -12,7 +12,7 @@ public class SensorSever : MonoBehaviour {
     [SerializeField]
     GameObject target;
     [SerializeField]
-    Text dataText;
+    Text infoText;
     [SerializeField]
     InputField address; //url
     [SerializeField]
@@ -26,12 +26,15 @@ public class SensorSever : MonoBehaviour {
     [SerializeField]
     private MqttEulerController mqttEuler;
     private EulerBlanding euler;
+    private EulerBlanding calibrateEuler;
     [SerializeField]
     Button button;
 
     void Start () {
         address.text = serverurl;
         warningText.text = "";
+        infoText.text = "device:null type:null";
+        consoleText.text = "console:";
         //StartCoroutine (ServerTest ());
         //express経由のデータ取得
         //StartCoroutine (GetSensorData ());
@@ -43,7 +46,7 @@ public class SensorSever : MonoBehaviour {
         mqttEuler.OnMessageReceived.Subscribe (message => {
             bool contain = message.Contains ("EH");
             if (!contain) return;
-            Debug.Log (message);
+            //Debug.Log (message);
             EulerBlanding data = JsonUtility.FromJson<EulerBlanding> (message);
             euler = data;
         });
@@ -52,71 +55,23 @@ public class SensorSever : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         deltatime += Time.deltaTime;
-        if (deltatime > 1.0f) {
-            //StartCoroutine (GetSensorData ());
-            //setTarget (axisdata);
 
-            deltatime -= 1.0f;
-        }
         if (euler != null) {
-            Debug.Log ("update");
+            infoText.text = "device:" + euler.devices + " type:" + euler.type;
             SetTargetEuler (euler);
         }
 
     }
-    //SensorData取得通信
-    IEnumerator GetSensorData () {
-        UnityWebRequest req = UnityWebRequest.Get (serverurl + "sensorData");
-        req.SetRequestHeader ("key", "KEY");
-        yield return req.SendWebRequest ();
-        if (req.isNetworkError) {
-            Debug.Log (req.error);
-        } else {
-            if (req.responseCode == 200) {
-                //OK
-                string jsonText = req.downloadHandler.text;
-                //Debug.Log (jsonText);
-                List<NineAxis> list = new List<NineAxis> ();
-                list = JsonUtility.FromJson<Serialize<NineAxis>> (jsonText).ToList ();
-                //Debug.Log (list[0].datetime);
-                setTarget (list[0]);
-            }
-        }
-    }
     //9軸オブジェクトへ適用
-    void setTarget (NineAxis data) {
-        //ax 0-10を想定
-        float AX = (data.ax * 18) - 90.0f;
-        //ay 0-10を想定
-        float AY = (data.ay * 18) + 90.0f;
-        //az 0-10を想定
-        float AZ = -((data.az * 18) - 90.0f);
-        //gx -1-1を想定
-        float GX = ((data.gx + 1.0f) * 180) - 90.0f;
-        //gy -1-1を想定
-        float GY = ((data.gy + 1.0f) * 180);
-        //gz -1-1を想定
-        float GZ = ((data.gz + 1.0f) * 180);
 
-        //Debug.Log ($"{data.ax:F3}");
-        //target.transform.rotation = Quaternion.Euler (90, 180, 0) * (new Quaternion (-data.gx, -data.gy, data.gz, 0));;
-        string datastr = "AX:" + $"{data.ax:F3}" + " AY:" + $"{data.ay:F3}" + " AZ:" + $"{data.az:F3}" + "\n";
-        datastr += "LX:" + $"{data.lx:F3}" + " LY:" + $"{data.ly:F3}" + " LZ:" + $"{data.lz:F3}" + "\n";
-        datastr += "GX:" + $"{data.gx:F3}" + " GY:" + $"{data.gy:F3}" + " GZ:" + $"{data.gz:F3}" + "\n";
-        dataText.text = datastr;
-    }
     void SetTargetEuler (EulerBlanding eulerdata) {
-        Debug.Log ("load");
-        Debug.Log (eulerdata.EP);
         float targetPitch = eulerdata.EP; // - calibrateEuler.pitch + 90.0f;
         float targetHead = eulerdata.EH; // - calibrateEuler.head;
         float targetRoll = eulerdata.ER; // - calibrateEuler.roll;
-        Debug.Log (target.transform.rotation);
         target.transform.rotation = Quaternion.Euler (
             targetPitch,
             targetHead,
             targetRoll);
-        Debug.Log ("set");
         if (eulerdata.EP < -30.0f) {
             warningText.text = "Warning!\n危険な体勢です！";
             spotLight.SetActive (true);
@@ -125,41 +80,12 @@ public class SensorSever : MonoBehaviour {
             spotLight.SetActive (false);
         }
     }
-    //Post通信テスト
-    IEnumerator PostServerData () {
-        WWWForm form = new WWWForm ();
-        //key:data
-        form.AddField ("myField", "myData");
-        form.AddField ("newxt", 20);
 
-        using (UnityWebRequest req = UnityWebRequest.Post (serverurl + "Post", form)) {
-            yield return req.SendWebRequest ();
-
-            if (req.isNetworkError) {
-                Debug.Log (req.error);
-            } else {
-                Debug.Log ("Form upload complete!");
-            }
-        }
-    }
-    IEnumerator ServerTest () {
-        consoleText.text = "No Server";
-        UnityWebRequest req = UnityWebRequest.Get (serverurl + "tests");
-        req.SetRequestHeader ("key", "KEY");
-        req.timeout = 10;
-        yield return req.SendWebRequest ();
-        if (req.isNetworkError) {
-            Debug.Log (req.error);
-            consoleText.text = req.error;
-        } else {
-            if (req.responseCode == 200) {
-                //OK
-            }
-        }
-        consoleText.text = "responceCode:" + req.responseCode.ToString ();
-    }
     public void OnClickSetServer () {
         serverurl = address.text;
-        StartCoroutine (ServerTest ());
+    }
+
+    public void SetConsole (string log) {
+        consoleText.text = "console:" + log;
     }
 }
